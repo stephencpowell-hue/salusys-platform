85b4af29ac18154bfd1287cf3bad5c738673e612
stephencpowell-hue <stephencpowell@gmail.com>
2026-02-02 10:22:29 -0600

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -14,7 +14,7 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-    base: "/salusys-platform/",
+  base: "/salusys-platform/",
   integrations: [
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
